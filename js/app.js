49e3642769136af257103741fe4f1b9c7b3d4f12
Bickeringmonkey <smeadows110280@gmail.com>
2021-06-24 18:10:05 +0100

    added comments

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
+// Score Variable - using Let so that the score can be added to.
 let score = 0;
-
+// userRank for storing the Rank the user recieves based on the score.. again using let so that the variable can be updated
 let userRank;
 
+// creating the variable to log the outcome to the screen, this uses const as we don't want this to change
 const main = document.querySelector('main');
+
+// 5 questions that will be asked in the prompt box, each consists of an if statement that will add 1 point for a correct answer. also uses toUpperCase() which makes all inputs to the prompt box turn into caps so that there is no issues with exact matches.
 
 const question1 = prompt("What film includes 'Face Huggers'?");
 if (question1.toUpperCase() === 'ALIENS') {
@@ -25,6 +29,7 @@ if (question5.toUpperCase() === 'JURASSIC PARK') {
   score += 1;
 }
 
+// if statements to update the userRank depending on what is logged in the score vaiable.
 if (score === 5) {
   userRank = 'Gold';
 } else if (score >= 3) {
@@ -35,5 +40,6 @@ if (score === 5) {
   userRank = 'no Rank';
 }
 
+// main variable with innerHTML attached which will display the content in between the main tags on the website.s
 main.innerHTML = `Congratulations, you have scored ${score} out of 5.
 You have achieved the rank of ${userRank}`;
